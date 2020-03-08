67e8a6db9343eaa83b3c8c03782df612a3f4ff16
Emma Hain <emmahain@gmail.com>
2020-03-08 19:16:43 +1000

    uploaded line_to_nearest.py

---
+++ line_to_nearest.py
@@ -0,0 +1,238 @@
+# -*- coding: utf-8 -*-
+
+"""
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************
+"""
+
+from qgis.PyQt.QtCore import QCoreApplication, QVariant
+from qgis.core import (QgsProcessing,
+                       QgsFeatureSink,
+                       QgsField,
+                       QgsFields,
+                       QgsFeature,
+                       QgsWkbTypes,
+                       QgsDistanceArea,
+                       QgsProcessingException,
+                       QgsProcessingAlgorithm,
+                       QgsProcessingParameterFeatureSource,
+                       QgsProcessingParameterFeatureSink,
+                       QgsProcessingUtils,
+                       QgsFeatureRequest)
+from qgis import processing
+
+
+class LineToNearestAlgorithm(QgsProcessingAlgorithm):
+    """
+    This is an example algorithm that takes a vector layer and
+    creates a new identical one.
+
+    It is meant to be used as an example of how to create your own
+    algorithms and explain methods and variables used to do it. An
+    algorithm like this will be available in all elements, and there
+    is not need for additional work.
+
+    All Processing algorithms should extend the QgsProcessingAlgorithm
+    class.
+    """
+
+    # Constants used to refer to parameters and outputs. They will be
+    # used when calling the algorithm from another algorithm, or when
+    # calling from the QGIS console.
+
+    INPUT = 'INPUT'
+    NEAREST = 'NEAREST'
+    OUTPUT = 'OUTPUT'
+
+    def tr(self, string):
+        """
+        Returns a translatable string with the self.tr() function.
+        """
+        return QCoreApplication.translate('Processing', string)
+
+    def createInstance(self):
+        return LineToNearestAlgorithm()
+
+    def name(self):
+        """
+        Returns the algorithm name, used for identifying the algorithm. This
+        string should be fixed for the algorithm, and must not be localised.
+        The name should be unique within each provider. Names should contain
+        lowercase alphanumeric characters only and no spaces or other
+        formatting characters.
+        """
+        return 'linetonearest'
+
+    def displayName(self):
+        """
+        Returns the translated algorithm name, which should be used for any
+        user-visible display of the algorithm name.
+        """
+        return self.tr('Create line to nearest feature')
+
+    def group(self):
+        """
+        Returns the name of the group this algorithm belongs to. This string
+        should be localised.
+        """
+        return self.tr('Emmas tools')
+
+    def groupId(self):
+        """
+        Returns the unique ID of the group this algorithm belongs to. This
+        string should be fixed for the algorithm, and must not be localised.
+        The group id should be unique within each provider. Group id should
+        contain lowercase alphanumeric characters only and no spaces or other
+        formatting characters.
+        """
+        return 'emma'
+
+    def shortHelpString(self):
+        """
+        Returns a localised short helper string for the algorithm. This string
+        should provide a basic description about what the algorithm does and the
+        parameters and outputs associated with it..
+        """
+        return self.tr("Creates a line to the nearest feature")
+
+    def initAlgorithm(self, config=None):
+        """
+        Here we define the inputs and output of the algorithm, along
+        with some other properties.
+        """
+
+        # We add the input vector features source. It can have any kind of
+        # geometry.
+        self.addParameter(
+            QgsProcessingParameterFeatureSource(
+                self.INPUT,
+                self.tr('Input layer'),
+                [QgsProcessing.TypeVectorAnyGeometry]
+            )
+        )
+        self.addParameter(
+            QgsProcessingParameterFeatureSource(
+                self.NEAREST,
+                'Join to nearest feature in',
+                [QgsProcessing.TypeVectorAnyGeometry]
+            )
+        )
+        
+        # We add a feature sink in which to store our processed features (this
+        # usually takes the form of a newly created vector layer when the
+        # algorithm is run in QGIS).
+        self.addParameter(
+            QgsProcessingParameterFeatureSink(
+                self.OUTPUT,
+                self.tr('Output layer')
+            )
+        )
+
+    def processAlgorithm(self, parameters, context, feedback):
+        """
+        Here is where the processing itself takes place.
+        """
+
+        # Retrieve the feature source and sink. The 'dest_id' variable is used
+        # to uniquely identify the feature sink, and must be included in the
+        # dictionary returned by the processAlgorithm function.
+        source = self.parameterAsSource(
+            parameters,
+            self.INPUT,
+            context
+        )
+        nearest_source = self.parameterAsSource(
+            parameters,
+            self.NEAREST,
+            context
+        )
+        
+        # If source was not found, throw an exception to indicate that the algorithm
+        # encountered a fatal error. The exception text can be any string, but in this
+        # case we use the pre-built invalidSourceError method to return a standard
+        # helper text for when a source cannot be evaluated
+        if source is None:
+            raise QgsProcessingException(self.invalidSourceError(parameters, self.INPUT))
+        if nearest_source is None:
+            raise QgsProcessingException(self.invalidSourceError(parameters, self.NEAREST))
+            
+        output_fields = QgsProcessingUtils.combineFields(source.fields(), nearest_source.fields(), 'nearest_')
+        output_fields.append(QgsField('distance', QVariant.Double))
+        
+        
+        # create an empty layer for the results to go into
+        (sink, dest_id) = self.parameterAsSink(
+            parameters,
+            self.OUTPUT,
+            context,
+            output_fields,
+            QgsWkbTypes.LineString,
+            source.sourceCrs()
+        )
+
+        # If sink was not created, throw an exception to indicate that the algorithm
+        # encountered a fatal error. The exception text can be any string, but in this
+        # case we use the pre-built invalidSinkError method to return a standard
+        # helper text for when a sink cannot be evaluated
+        if sink is None:
+            raise QgsProcessingException(self.invalidSinkError(parameters, self.OUTPUT))
+
+        # Compute the number of steps to display within the progress bar and
+        # get features from source
+        total = 100.0 / source.featureCount() if source.featureCount() else 0
+        features = source.getFeatures()
+        
+        da = QgsDistanceArea()
+        da.setEllipsoid(context.project().ellipsoid())
+        da.setSourceCrs(source.sourceCrs(), context.transformContext())
+
+        for current, feature in enumerate(features):
+            # Stop the algorithm if cancel button has been clicked
+            if feedback.isCanceled():
+                break
+                
+            # feature is our polygon feature we need to join to something else
+            # find the nearest feature in the other layer
+            
+            # create a line which joins ours polygon to the nearest feature
+            # (shortest line possible!)
+    
+            shortest_line = None
+            shortest_line_length = 99999999999999
+            closest_feature = None
+            for candidate_feature in nearest_source.getFeatures(QgsFeatureRequest().setDestinationCrs(source.sourceCrs(), context.transformContext())):
+                line = feature.geometry().shortestLine(candidate_feature.geometry())
+                line_length =da.measureLength(line)
+                if line_length < shortest_line_length:
+                    shortest_line = line
+                    shortest_line_length = line_length
+                    closest_feature = candidate_feature
+            
+            # now shortest_line is the best one!
+            # create the output feature
+            output_feature = QgsFeature()
+            output_feature.setGeometry(shortest_line)
+            attrs = feature.attributes()
+            attrs.extend(closest_feature.attributes())
+            attrs.append(shortest_line_length)
+            output_feature.setAttributes(attrs)
+
+            # Add a feature in the sink
+            sink.addFeature(output_feature, QgsFeatureSink.FastInsert)
+
+            # Update the progress bar
+            feedback.setProgress(int(current * total))
+
+        # Return the results of the algorithm. In this case our only result is
+        # the feature sink which contains the processed features, but some
+        # algorithms may return multiple feature sinks, calculated numeric
+        # statistics, etc. These should all be included in the returned
+        # dictionary, with keys matching the feature corresponding parameter
+        # or output names.
+        return {self.OUTPUT: dest_id}
